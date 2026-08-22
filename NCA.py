2dd49d2e6d9fabdcac9fa4b8e78cb646ca85adef
lshofsl <130612237+lshofsl@users.noreply.github.com>
2026-08-22 22:21:07 +0200

    Correct call

--- NCA.py
+++ NCA.py
@@ -45,106 +45,11 @@ def masked_perception(x, mask_n=0):
 
 
 def reduced_perception(x, mask_n=0):
-
     filters = torch.stack([sobel_x, sobel_x.T, lap])
     x_redu = x[:,0:x.shape[1]-mask_n]
     obs = perchannel_conv(x_redu,filters)
     return torch.cat((x,obs), dim = 1 )
     
-
-
-class DummyVCA(torch.nn.Module):
-    def __init__(self, chn=12, hidden_n=96, mask_n=0):
-        super().__init__()
-        self.chn = chn
-        self.w1 = torch.nn.Conv2d(4 * chn, hidden_n, 1)
-        self.w2 = torch.nn.Conv2d(hidden_n, chn, 1, bias=False)
-        self.w2.weight.data.zero_()
-        self.mask_n = mask_n
-
-    def forward(self, x, update_rate=0.5):
-        y = perception(x, self.mask_n)
-        y = self.w2(torch.relu(self.w1(y)))
-        b, c, h, w = y.shape
-        update_mask = (torch.rand(b, 1, h, w, device="cuda:0") + update_rate).floor()
-        xmp  = torch.nn.functional.pad(x[:, None, 3, ...],pad = [1,1,1,1] ,mode= "circular")
-        pre_life_mask = torch.nn.functional.max_pool2d(xmp, 3, 1, 0,).cuda() > 0.1
-        # Perform update
-        x = x + y * update_mask * pre_life_mask
-        return x
-
-class MaskedCA(torch.nn.Module):
-    def __init__(self, chn=12, hidden_n=96, mask_n=0):
-        super().__init__()
-        self.chn = chn
-        self.w1 = torch.nn.Conv2d(4 * chn, hidden_n, 1)
-        self.w2 = torch.nn.Conv2d(hidden_n, chn, 1, bias=False)
-        self.w2.weight.data.zero_()
-        self.mask_n = mask_n
-
-    def forward(self, x, update_rate=0.5):
-        y = masked_perception(x, self.mask_n)
-        y = self.w2(torch.relu(self.w1(y)))
-        b, c, h, w = y.shape
-        update_mask = (torch.rand(b, 1, h, w, device="cuda:0") + update_rate).floor()
-        xmp  = torch.nn.functional.pad(x[:, None, 3, ...],pad = [1,1,1,1] ,mode= "circular")
-        pre_life_mask = torch.nn.functional.max_pool2d(xmp, 3, 1, 0).cuda() > 0.1
-        # Perform update
-        x = x + y * update_mask * pre_life_mask
-        return x
-
-
-class ReducedCA(torch.nn.Module):
-    def __init__(self, chn=12, hidden_n=96, mask_n=0):
-        super().__init__()
-        self.chn = chn
-        self.w1 = torch.nn.Conv2d(chn + 3*(chn-  mask_n), hidden_n, 1)
-        self.w2 = torch.nn.Conv2d(hidden_n, chn, 1, bias=False)
-        self.w2.weight.data.zero_()
-        self.mask_n = mask_n
-
-    def forward(self, x, update_rate=0.5):
-        y = reduced_perception(x, self.mask_n)
-        y = self.w2(torch.relu(self.w1(y)))
-        b, c, h, w = y.shape
-        update_mask = (torch.rand(b, 1, h, w, device="cuda:0") + update_rate).floor()
-        xmp  = torch.nn.functional.pad(x[:, None, 3, ...],pad = [1,1,1,1] ,mode= "circular")
-        pre_life_mask = torch.nn.functional.max_pool2d(xmp, 3, 1, 0,).cuda() > 0.1
-        # Perform update
-        x = x + y * update_mask * pre_life_mask
-        return x
-
-
-class GeneCA(torch.nn.Module):
-    def __init__(self, chn=12, hidden_n=96, gene_size=3):
-        super().__init__()
-        self.chn = chn
-        self.gene_size = gene_size 
-        self.beta = torch.nn.Parameter(torch.zeros(1))
-        self.w1 = torch.nn.Conv2d(chn + 3 * (chn), hidden_n, 1)
-        self.public = chn  - gene_size  
-        self.w2 = torch.nn.Conv2d(hidden_n, self.public, 1, bias=False)
-        self.w2.weight.data.zero_()
-        
-
-    def forward(self, x, update_rate=0.5):
-        gene = x[:, -self.gene_size:, ...]
-        s = x[:, :self.public, ...]
-        y = reduced_perception(x[:, :self.chn], 0)   #Only perceive the RGBA + hidden + genes 
-        
-        ##NCA state update 
-        delta_s = self.w2(torch.relu(self.w1(y)))
-        b, c, h, w = y.shape
-        ## Energy function: quadratic Hopfield form
-        energy_grad = -s
-        update_mask = (torch.rand(b, 1, h, w, device=x.device) + update_rate).floor()
-        xmp = torch.nn.functional.pad(x[:, None, 3, ...], pad=[1, 1, 1, 1], mode="circular")
-        pre_life_mask = torch.nn.functional.max_pool2d(xmp, 3, 1, 0, ).cuda() > 0.1
-        s_update = s + (delta_s * update_mask - self.beta * energy_grad) * pre_life_mask
-        x = torch.cat((s_update, gene), dim=1)
-        return x
-
-
 
 class EnergyNCA(nn.Module):
     def __init__(self, chn=16):
@@ -291,7 +196,7 @@ class HYBRID_NCA(torch.nn.Module):
         return W_sym
         
     def MLP(self, x): 
-        y = reduce_perception(x) # compute spatial perception
+        y = reduced_perception(x) # compute spatial perception
         y = self.w2(F.relu(self.w1(y))) # pass tensor y through layers
         return y
         
